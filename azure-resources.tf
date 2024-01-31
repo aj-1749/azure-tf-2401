12dfb887111ccffcfb52c2503077a683f807ad15
ravi2krishna <ravi2krishna@gmail.com>
2024-01-31 10:44:43 +0530

    create vnet

--- azure-resources.tf
+++ azure-resources.tf
@@ -6,3 +6,12 @@ resource "azurerm_resource_group" "ado-2401-rg" {
     env = "dev"
   }
 }
+
+# Virtual Network 
+resource "azurerm_virtual_network" "ado-vnet" {
+  name                = "ado-network"
+  location            = azurerm_resource_group.ado-2401-rg.location
+  resource_group_name = azurerm_resource_group.ado-2401-rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
